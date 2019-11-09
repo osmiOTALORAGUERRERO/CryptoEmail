e36ad4ec0feea12336a403aa89b1363e0616d8bb
Manuela <mgarciamons@gmail.com>
2019-11-09 21:19:59 +0100

    views

---
+++ vistas/principal.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>CryptoEmail</title>
+  </head>
+  <h1 class=" titulo">CryptoEmail</h1>
+  <body>
+
+  </body>
+</html>
