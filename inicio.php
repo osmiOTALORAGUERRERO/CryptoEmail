1fd17d85e5ae586c2c53b1ce2eccf6a98bed8de2
Manuela <mgarciamons@gmail.com>
2019-11-09 20:45:38 +0100

    agregando login

---
+++ inicio.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>CrypoEmail</title>
+    <link rel="stylesheet" href="/estilos/index.css">
+  </head>
+  <body>
+    <div class="login-box">
+      <h1>Login Here</h1>
+      <form>
+        <!-- USERNAME INPUT -->
+        <label for="username">Username</label>
+        <input type="text" placeholder="Enter Username">
+        <!-- PASSWORD INPUT -->
+        <label for="password">Password</label>
+        <input type="password" placeholder="Enter Password">
+        <input type="submit" value="Log In">
+        <a href="#">Lost your Password?</a><br>
+        <a href="#">Don't have An account?</a>
+      </form>
+    </div>
+  </body>
+</html>
