e653df016326cf5d0304c841c86b94e1e5deeae7
Manuela <mgarciamons@gmail.com>
2019-11-11 15:02:54 +0100

    cambios en principal

--- vistas/principal.php
+++ vistas/principal.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <meta charset="utf-8">
+      <meta charset="utf-8">
     <title>CryptoEmail</title>
+    <link rel="stylesheet"  type="text/css" href="/estilos/principal.css">
   </head>
   <h1 class=" titulo">CryptoEmail</h1>
   <body>
-
+    <div id="menu">
+   <ul>
+    <li><a href="..." title="...">Inicio</a></li>
+    <li><a href="..." title="...">Mis Correos</a></li>
+    <li><a href="..." title="...">Enviados</a></li>
+    <li><a href="..." title="...">Redactar</a></li>
+   </ul>
+</div>
   </body>
 </html>
