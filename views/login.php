ee7ce0b6b155500221ab6713e8151cbd6093fbeb
Osmi Otalora <santiago.ooo6@gmail.com>
2019-11-12 11:33:42 -0500

    mejorando interfaces y añadiendo write y messages

--- views/login.php
+++ views/login.php
@@ -17,11 +17,6 @@
 		<div class="card">
 			<div class="card-header">
 				<h3>Sign In</h3>
-				<div class="d-flex justify-content-end social_icon">
-					<span><i class="fab fa-facebook-square"></i></span>
-					<span><i class="fab fa-google-plus-square"></i></span>
-					<span><i class="fab fa-twitter-square"></i></span>
-				</div>
 			</div>
 			<div class="card-body">
 				<form>
@@ -38,9 +33,6 @@
 						</div>
 						<input type="password" class="form-control" placeholder="password">
 					</div>
-					<div class="row align-items-center remember">
-						<input type="checkbox">Remember Me
-					</div>
 					<div class="form-group">
 						<input type="submit" value="Login" class="btn float-right login_btn">
 					</div>
@@ -49,9 +41,6 @@
 			<div class="card-footer">
 				<div class="d-flex justify-content-center links">
 					Don't have an account?<a href="#">Sign Up</a>
-				</div>
-				<div class="d-flex justify-content-center">
-					<a href="#">Forgot your password?</a>
 				</div>
 			</div>
 		</div>
