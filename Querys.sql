726cecb2561fbf483dedb4350ac16701ef3ee171
Osmi Otalora <santiago.ooo6@gmail.com>
2019-11-19 10:43:01 -0500

    añadiendo querys necesarios para el modelo y los datos de prueba

---
+++ Querys.sql
@@ -0,0 +1,21 @@
+#Para traer el usuario que necesito login
+SELECT * FROM crypto_email.users WHERE email="dyg9812@gmail.com";
+#Para traer los mensajes recividos del usuario logueado
+SELECT * FROM crypto_email.receivedmessages 
+    INNER JOIN crypto_email.messages ON receivedmessages.idMessage = messages.id
+    INNER JOIN crypto_email.users ON receivedmessages.`from` = users.id 
+	WHERE idUser = 1;
+#Para traer los mensajes enviados del usuario logueado
+SELECT * FROM crypto_email.sentmessages 
+    INNER JOIN crypto_email.messages ON sentmessages.idMessage = messages.id
+    INNER JOIN crypto_email.users ON sentmessages.`for` = users.id 
+	WHERE idUser = 1;
+#Para traer los usuario disponibles que el usuario logueado tiene para enviar un correo
+SELECT id, name, email FROM crypto_email.users;
+#-----------------------------------------------------------------------------------------#
+
+#Para enviar un correo del usuario logueado a otra
+INSERT INTO `crypto_email`.`messages` (`transmitter`, `receiver`, `message`) VALUES ('3', '3', 'Este mensaje es para el mismo alberto');
+INSERT INTO `crypto_email`.`receivedmessages` (`idUser`, `idMessage`, `from`) VALUES ('3', '6', '3');
+INSERT INTO `crypto_email`.`sentmessages` (`idUser`, `idMessage`, `for`) VALUES ('3', '6', '3');
+
